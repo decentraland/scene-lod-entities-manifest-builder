553a38b49187f68bdc8ede28ef9a802829a72c08
Juan Molteni <juanignaciomolteni@gmail.com>
2024-01-03 11:22:54 -0300

    Build scene by ID

--- src/logic/sceneFetcher.ts
+++ src/logic/sceneFetcher.ts
@@ -10,7 +10,7 @@ export let sdk6FetchComponent: any
 export let mainCrdt: any
 export let sceneId: string = 'local-scene' // will get overwritten if a remote scene is targeted
 
-export async function getGameDataFromRemoteScene(fetch: IFetchComponent, sceneCoords: string): Promise<string> {  
+export async function getGameDataFromRemoteSceneByCoords(fetch: IFetchComponent, sceneCoords: string): Promise<string> {
   // get scene id
   let fetchResponse = await fetch.fetch(mappingsUrl, {
     method: 'post',
@@ -18,13 +18,26 @@ export async function getGameDataFromRemoteScene(fetch: IFetchComponent, sceneCo
     body: JSON.stringify({ pointers: [sceneCoords] })
   })
   const sceneData = (await fetchResponse.json())[0]
-  sceneId = sceneData.id
-  
+  sceneId = sceneData.id;
+  return await processRemoteResponse(fetch, sceneData)
+}
+
+export async function getGameDataFromRemoteSceneByID(fetch: IFetchComponent, paramSceneId: string): Promise<string> {
+  sceneId = paramSceneId
+  let fetchResponse = await fetch.fetch(`${contentFetchBaseUrl}${sceneId}`, {
+    method: 'get',
+    headers: { 'content-type': 'application/json' }
+  })
+  const sceneData = (await fetchResponse.json())
+  return await processRemoteResponse(fetch, sceneData)
+}
+
+export async function processRemoteResponse(fetch: IFetchComponent, sceneData : any) : Promise<string>{
   if (!process.env.npm_config_overwrite && existsSync(`${manifestFileDir}/${sceneId}${manifestFileNameEnd}`)) {
     throw new Error(`ABORT: ${sceneId}${manifestFileNameEnd} manifest file already exists.`)
   }
-  
-  console.log(`Fetched scene data - coords: ${sceneCoords}; scene id:${sceneId}; sdk7? ${sceneData.metadata.runtimeVersion === '7'}`)
+
+  console.log(`Fetched scene data scene id:${sceneId}; sdk7? ${sceneData.metadata.runtimeVersion === '7'}`)
 
   // SDK6 scenes support
   if (sceneData.metadata.runtimeVersion !== '7') {
@@ -32,32 +45,33 @@ export async function getGameDataFromRemoteScene(fetch: IFetchComponent, sceneCo
     sdk6SceneContent = sceneData.content
     sdk6FetchComponent = fetch
 
-    fetchResponse = await fetch.fetch(`https://renderer-artifacts.decentraland.org/sdk7-adaption-layer/main/index.js`)
+    let fetchResponse = await fetch.fetch(`https://renderer-artifacts.decentraland.org/sdk7-adaption-layer/main/index.js`)
     return await fetchResponse.text()
   }
 
   // SDK7 editor-made scenes support (main.crdt binary file)
   const sceneMainCRDTFileHash = sceneData.content.find(($: any) => $.file === mainCRDTFileName)?.hash
   if (sceneMainCRDTFileHash) {
-    fetchResponse = await fetch.fetch(`${contentFetchBaseUrl}${sceneMainCRDTFileHash}`)
+    let fetchResponse = await fetch.fetch(`${contentFetchBaseUrl}${sceneMainCRDTFileHash}`)
     mainCrdt = new Uint8Array(await fetchResponse.arrayBuffer())
   }
-  
+
   // Get SDK7 scene main file (index.js/game.js)
   const sceneMainFileHash = sceneData.content.find(($: any) => $.file === sceneData.metadata.main)?.hash
   if (!sceneMainFileHash) {
     throw new Error(`ABORT: Cannot find scene's main asset file.`)
   }
-  fetchResponse = await fetch.fetch(`${contentFetchBaseUrl}${sceneMainFileHash}`)
-  
+  let fetchResponse = await fetch.fetch(`${contentFetchBaseUrl}${sceneMainFileHash}`)
+
   return await fetchResponse.text()
 }
+
 
 // Local scenes mode only supports SDK7 scenes for now
 export async function getGameDataFromLocalScene(scenePath: string): Promise<string> {
   if (!process.env.npm_config_overwrite && existsSync(`${manifestFileDir}/${sceneId}${manifestFileNameEnd}`)) {
     throw new Error(`ABORT: ${sceneId}${manifestFileNameEnd} manifest file already exists.`)
   }
-  
+
   return readFileSync(scenePath, 'utf-8')
 }
--- src/logic/sceneLoader.ts
+++ src/logic/sceneLoader.ts
@@ -1,15 +1,19 @@
 import { createSceneComponent } from '../adapters/scene'
-import { getGameDataFromLocalScene, getGameDataFromRemoteScene } from './sceneFetcher'
+import {getGameDataFromLocalScene, getGameDataFromRemoteSceneByCoords, getGameDataFromRemoteSceneByID} from './sceneFetcher'
 import { BaseComponents } from '../types'
 
-export async function loadOrReload({ config, fetch }: BaseComponents, loadingType: string, targetScene: string) {
+export async function loadOrReload({ config, fetch }: BaseComponents, loadingType: string, targetScene: string, doneBySceneID : boolean) {
   let hash: string
   let sourceCode: string
   if (loadingType === 'localScene') {
     sourceCode = await getGameDataFromLocalScene(targetScene)
     hash = 'localScene'
   } else {
-    sourceCode = await getGameDataFromRemoteScene(fetch, targetScene)
+    if(doneBySceneID){
+      sourceCode = await getGameDataFromRemoteSceneByID(fetch, targetScene)
+    }else{
+      sourceCode = await getGameDataFromRemoteSceneByCoords(fetch, targetScene)
+    }
     hash = 'remoteScene'
   }
 
