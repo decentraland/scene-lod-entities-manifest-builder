e419294644857b599a739e0999063e5f36b5eb53
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2025-10-21 11:32:52 -0300

    chore: Update branch to latest state for ABConverter (#3)

--- src/adapters/scene.ts
+++ src/adapters/scene.ts
@@ -1,8 +1,10 @@
 import { customEvalSdk7 } from '../logic/scene-runtime/sandbox'
 import { createModuleRuntime } from '../logic/scene-runtime/sdk7-runtime'
 import { setTimeout } from 'timers/promises'
+import { initSourcemap } from '../logic/sourcemap'
 
-const FRAMES_TO_RUN = 60
+export const FRAMES_TO_RUN = 91
+export let framesCount = 1
 
 export type ISceneComponent = {
   start(hash: string, sourceCode: string): Promise<void>
@@ -15,21 +17,30 @@ export async function createSceneComponent(): Promise<ISceneComponent> {
   async function start(hash: string, sourceCode: string) {
     abortController = new AbortController()
     loaded = true
-
     const runtimeExecutionContext = Object.create(null)
     const sceneModule = createModuleRuntime(runtimeExecutionContext)
-
     try {
       await customEvalSdk7(sourceCode, runtimeExecutionContext)
-      const updateIntervalMs: number = 1000 / 30
 
-      await sceneModule.runStart()
+      // This only works for scenes that has the sourcemap uploaded inside the .js file or local scenes.
+      const sourceMap = await initSourcemap(sourceCode, true)
+      //30 FPS
+      const updateIntervalMs: number = 33.33
+
+      try {
+        await sceneModule.runStart()
+      } catch (e: any) {
+        console.log('[Start failed]: ', sourceMap.parseError(e))
+      }
 
       // start event loop
       if (sceneModule.exports.onUpdate) {
         // first update always use 0.0 as delta time
-        await sceneModule.runUpdate(0.0)
-        let framesCount = 1
+        try {
+          await sceneModule.runUpdate(0.0)
+        } catch (e: any) {
+          console.log('[Update failed]: ', sourceMap.parseError(e))
+        }
         let start = performance.now()
 
         while (framesCount < FRAMES_TO_RUN) {
@@ -40,11 +51,10 @@ export async function createSceneComponent(): Promise<ISceneComponent> {
           const dtSecs = dtMillis / 1000
 
           await sceneModule.runUpdate(dtSecs)
-          
+
           // wait for next frame
           const ms = Math.max((updateIntervalMs - (performance.now() - start)) | 0, 0)
           await setTimeout(Math.max(ms | 0, 0), undefined, { signal: abortController.signal })
-          
           framesCount++
         }
       }
@@ -62,6 +72,6 @@ export async function createSceneComponent(): Promise<ISceneComponent> {
   }
 
   return {
-    start,
+    start
   }
 }
--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,7 @@
 import { Lifecycle } from '@well-known-components/interfaces'
 import { BaseComponents } from './types'
 import { loadOrReload } from './logic/sceneLoader'
+import { setManifestFileDir } from './logic/scene-runtime/apis'
 
 // this function wires the business logic (adapters & controllers) with the components (ports)
 export async function main(program: Lifecycle.EntryPointParameters<BaseComponents>) {
@@ -8,12 +9,18 @@ export async function main(program: Lifecycle.EntryPointParameters<BaseComponent
 
   // start ports: db, listeners, synchronizations, etc
   await startComponents()
+
+  // Configure output manifest directory
+  const outputDir = process.env.npm_config_output ?? await components.config.getString('OUTPUT_MANIFEST_DIR')
+  if (outputDir) {
+    setManifestFileDir(outputDir)
+  }
   
   const localPath = process.env.npm_config_path ?? await components.config.getString('LOCAL_SCENE_PATH')
   if (localPath) {
     await loadOrReload(components, 'localScene', localPath, false)
   } else {
-    const sceneID = process.env.npm_config_sceneID
+    const sceneID = process.env.npm_config_sceneid
     if (sceneID) {
       await loadOrReload(components, 'remoteScene', sceneID, true)
     }else{
